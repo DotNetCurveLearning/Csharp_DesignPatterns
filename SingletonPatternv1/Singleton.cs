501ae9998c49572aea29c75795cb8089d303f4c9
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2023-01-20 17:32:52 +0100

    Singleton Pattern thread safe implementation

---
+++ SingletonPatternv1/Singleton.cs
@@ -0,0 +1,31 @@
+﻿namespace SingletonPatternv1
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance = null;
+        private static readonly object padlock = new object();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null) // only get a lock if the instance is null
+                {
+                    lock (padlock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Singleton();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private Singleton()
+        {
+            // cannot be created except within this class
+        }
+    }
+}
